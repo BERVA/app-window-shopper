4c2f61bceb72a44530dc50c7860c6665a11060e6
Barış Cesim <berva@Bars-MacBook-Pro.local>
2017-11-29 21:03:52 +0300

    Checkpoint

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Barış Cesim on 29.11.2017.
+//  Copyright © 2017 bcesim. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
